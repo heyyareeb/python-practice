9b7f11920e4529ed12058427a8a1f7e819e6f797
heyyareeb <135145802+heyyareeb@users.noreply.github.com>
2026-04-03 12:06:22 +0530

    Added Day 4 Rock Paper Scissors project

---
+++ rock_paper_scissors.py
@@ -0,0 +1,56 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+
+game = ('rock', 'paper' , 'scissors')
+
+user = int(input("What do you choose? type 0 for rock, 1 for paper, 2 for scissors.\n"))
+print("you chose--",game[user])
+
+computer = random.randint(0,2)
+print("computer chose--",game[computer])
+
+if user >=3 or user <0:
+    print("Invalid option")
+elif user == computer:
+    print("Draw")
+elif user == 0 and computer == 1:
+    print("You Lose")
+elif user == 1 and computer == 0:
+    print("You Win")
+elif user == 1 and computer == 2:
+    print("You Lose")
+elif user == 2 and computer == 1:
+    print("You win")
+elif user == 2 and computer == 0:
+    print("you Lose")
+elif user == 0 and computer == 2:
+    print("You win")
+else:
+    print ("you have entered an invalid option")
